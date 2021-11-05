2c6769a22d579506dbc659440b62c4dc610635ea
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2021-11-05 17:02:17 +0700

    Lecture 9

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  final String categoryId;
-  final String categoryTitle;
+  /*Không dùng cái này nữa vì chuyển sang dùng named route*/
+  // final String categoryId;
+  // final String categoryTitle;
 
-  CategoryMealsScreen(this.categoryId, this.categoryTitle);
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
 
   @override
   Widget build(BuildContext context) {
+    //Sửa 1 chút so với Code mẫu do có lỗi:
+    //https://stackoverflow.com/questions/67659678/flutter-the-property-settings-cant-be-unconditionally-accessed-because-the
+    final routeArgs =
+        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(categoryTitle),
+        //phải convert kiểu
+        title: Text(categoryTitle as String),
       ),
       body: Center(
         child: Text('The recipes for the Category!'),
