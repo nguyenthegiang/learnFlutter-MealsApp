ab64ec751f0431b00a7e209df611c1227ce29917
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2021-11-05 15:17:19 +0700

    Initial Commit - Lecture 6

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  const CategoryMealsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('The recipes for the Category!'),
+    );
+  }
+}
