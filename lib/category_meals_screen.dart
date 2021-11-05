777119de7d481fb74737bcc30dd3e71290e70762
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2021-11-05 16:11:17 +0700

    Lecture 8

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  const CategoryMealsScreen({Key? key}) : super(key: key);
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealsScreen(this.categoryId, this.categoryTitle);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('The recipes for the Category!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: Center(
+        child: Text('The recipes for the Category!'),
+      ),
     );
   }
 }
