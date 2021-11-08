776b6b1caf74ef0818166c98fc97819317de6f6a
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2021-11-08 11:28:52 +0700

    Lecture 14

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './dummy_data.dart';
+import '../widgets/meal_item.dart';
+import '../dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = 'category-meals';
@@ -34,7 +35,12 @@ class CategoryMealsScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return Text(categoryMeals[index].title);
+          return MealItem(
+              title: categoryMeals[index].title,
+              imageUrl: categoryMeals[index].imageUrl,
+              affordability: categoryMeals[index].affordability,
+              complexity: categoryMeals[index].complexity,
+              duration: categoryMeals[index].duration);
         },
         itemCount: categoryMeals.length,
       ),
--- lib/widgets/category_item.dart
+++ lib/widgets/category_item.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import './category_meals_screen.dart';
+import '../screens/category_meals_screen.dart';
 
 //Dùng cho cái GridView của Categories_screen
 class CategoryItem extends StatelessWidget {
