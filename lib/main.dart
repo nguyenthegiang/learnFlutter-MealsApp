af4c6b3d4fbec466aa3784f15fdfe7172a1ed67e
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2021-11-09 08:34:22 +0700

    Lecture 20

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
+import './screens/tabs_screen.dart';
 import './screens/meal_detai_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/categories_screen.dart';
@@ -34,7 +35,7 @@ class MyApp extends StatelessWidget {
       //home: CategoriesScreen(),
       initialRoute: '/',
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
---
+++ lib/screens/favorites_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FavoriesScreen extends StatelessWidget {
+  const FavoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('The Favorites'),
+    );
+  }
+}
