2df0d92e40f61a63ebcb3d4462b4d9cd3edf5f41
nguyenthegiang <giangnthe153046@fpt.edu.vn>
2021-11-09 11:11:44 +0700

    Lecture 25

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/main_drawer.dart';
+
 class FavoriesScreen extends StatelessWidget {
   const FavoriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('The Favorites'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Filter'),
+      ),
+      drawer: MainDrawer(),
+      body: Center(
+        child: Text('Filters'),
+      ),
     );
   }
 }
